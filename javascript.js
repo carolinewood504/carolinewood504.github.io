7684b0d5d57c0824b977fb1e4e4e4dd864cae168
carolinewood504 <carolinewood504@gmail.com>
2016-01-31 15:02:16 -0500

    Practice JS

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var temperature=25
+if(temperature < 50){console.log("wear a coat");}
+else if(temperature<0){console.log("Stay inside");}
+else if(temperature <30){console.log("Wear a coat and a hat");}
+else{console.log("wear whatever you want");}
+
